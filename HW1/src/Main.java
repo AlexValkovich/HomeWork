c0c32e475ea0b4c4efd6568f6be86450d9fceb20
AlexValkovich <aleksandr199730@mail.ru>
2024-02-25 16:07:33 +0300

    Create Main.java

---
+++ HW1/src/Main.java
@@ -0,0 +1,17 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int n = 25;
+        if (n%2 == 0) {
+            System.out.println("n" + " Четное");
+        }
+            else { System.out.println("n" + " Нечетное");
+            }
+
+
+
+
+
+    }
+}
