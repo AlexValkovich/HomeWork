c30655b5a507b7850f58355ff5d758636409c510
AlexValkovich <aleksandr199730@mail.ru>
2024-02-25 16:08:29 +0300

    Create Main.java

---
+++ HW2 switch/src/Main.java
@@ -0,0 +1,37 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println ("Введите число от 1 до 7");
+        int d = scan.nextInt();
+        switch (d){
+            case 1:
+            System.out.println ("Понедельник");
+            break;
+            case 2:
+            System.out.println ("Вторник");
+            break;
+            case 3:
+                System.out.println ("Среда");
+                break;
+            case 4:
+                System.out.println ("Четверг");
+                break;
+            case 5:
+                System.out.println ("Пятница");
+                break;
+            case 6:
+                System.out.println ("Суббота");
+                break;
+            case 7:
+                System.out.println ("Воскресенье");
+                break;
+            default:
+                System.out.println ("Неизвестное число!!!");
+                break;
+        }
+
+    }
+}
