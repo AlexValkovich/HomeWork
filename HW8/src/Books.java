b6c6e5238cd39c8808216fbf1dd470261ea51703
AlexValkovich <aleksandr199730@mail.ru>
2024-03-17 14:31:35 +0300

    Create Books.java

---
+++ HW8/src/Books.java
@@ -0,0 +1,14 @@
+public class Books {
+    public String bookName;
+    public String authorName;
+    public final String TEXT = " ";
+
+    public Books(String bookName, String authorName) {
+        this.bookName = bookName;
+        this.authorName = authorName;
+    }
+
+    public Books(String bookName) {
+        this.bookName = bookName;
+    }
+}
